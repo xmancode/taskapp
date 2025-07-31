0454f5d1716fd73b378c2127dd51fc6e6ab8fda7
xmancode <sarathgoc@gmail.com>
2025-07-31 22:30:15 +0530

    started and create a tabs for task app

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,39 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
+import { Ionicons } from '@expo/vector-icons'
+import { Tabs } from 'expo-router'
+import React from 'react'
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+const TabsLayout = () => {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
+    <Tabs screenOptions={{
+        tabBarActiveTintColor:"#f13b17ff",
+        tabBarInactiveTintColor:"#253153ff",
+        tabBarStyle:{
+            backgroundColor:"#cac7c8ff",
+            borderTopWidth:1,
+            borderTopColor:"#343631ff",
+            height:60,
+            paddingTop:5,
+            paddingBottom:2
+        },
+        tabBarLabelStyle:{
+            fontSize:12,
+            fontWeight:'500'
+        }
+    }}>
+    <Tabs.Screen name='index' options={{
+        title:"Todos",
+        tabBarIcon:({color,size})=>(
+            <Ionicons name='today-outline' size={size} color={color}/>
+        )
+        }}/>
+    <Tabs.Screen name='settings' options={{
+        title:"Settings",
+        tabBarIcon:({color,size})=>(
+            <Ionicons name='settings-outline' size={size} color={color}/>
+        )
+        }}/>
     </Tabs>
-  );
+  )
 }
+
+export default TabsLayout
