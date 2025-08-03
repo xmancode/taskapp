152d5ca5061ec922bf4e8e178093a6960a9cecc9
xmancode <sarathgoc@gmail.com>
2025-08-03 22:19:38 +0530

    added todos completed icon for progression

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,65 @@
 
 import { createHomeStyles } from "@/assets/styles/home.styles";
 import Header from "@/components/Header";
+import LoadingSpinner from "@/components/LoadingSpinner";
 import TodoInput from "@/components/TodoInput";
+import { api } from "@/convex/_generated/api";
+import { Doc, Id } from "@/convex/_generated/dataModel";
 import useTheme from "@/hooks/useTheme";
+import { Ionicons } from "@expo/vector-icons";
+import { useMutation, useQuery } from "convex/react";
 import { LinearGradient } from 'expo-linear-gradient';
-import { StatusBar } from "react-native";
+import { Alert, FlatList, StatusBar, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-
+type Todo=Doc<"todos">
 export default function Index() {
   const {toggleDarkMode,colors}=useTheme();
    const homeStyles=createHomeStyles(colors);
+   const todos=useQuery(api.todos.getTodos);
+   // todos is undefined when loading
+
+   const toggleTodo=useMutation(api.todos.toggleTodo);
+
+   let loading= (todos == undefined) ;
+   if(loading) return <LoadingSpinner/> ;
+
+  const handleToggleTodo= async (id:Id<"todos">)=>{
+    try {
+      await  toggleTodo({id})
+      
+    } catch (error) {
+      console.log("error, to toggle",error)
+      Alert.alert("failed to toggle todos");
+      
+    }
+  }
+
+   const renderTodoItem=({item}:{item:Todo})=>{
+    return <View style={homeStyles.todoItemWrapper}>
+      <LinearGradient colors={colors.gradients.surface}
+      style={homeStyles.todoItem}
+      start={{x:0,y:0}}
+      end={{x:1,y:1}}
+      >
+     <TouchableOpacity
+        style={homeStyles.checkbox}
+        activeOpacity={0.7}
+        onPress={()=>handleToggleTodo(item._id)}
+        >
+        <LinearGradient colors={item.isCompleted? colors.gradients.success:
+          colors.gradients.muted}
+          style={[homeStyles.checkboxInner,
+            {borderColor:item.isCompleted? "transparent":
+            colors.border},]}>
+            { item.isCompleted && <Ionicons name="checkmark" size={18} color="#fff"/>}
+
+        </LinearGradient>
+         </TouchableOpacity>
+      </LinearGradient>
+          
+    </View>
+   }
 
   return (
     
@@ -19,6 +68,15 @@ export default function Index() {
     <SafeAreaView style={homeStyles.safeArea}>
       <Header/>
      <TodoInput/>
+     {/* {todos?.map((todo)=> <Text key={todo._id}>{todo.text}</Text>
+
+     )} */}
+     <FlatList data={todos}
+      renderItem={renderTodoItem}
+      keyExtractor={(item)=>item._id}
+      style={homeStyles.todoList}
+      contentContainerStyle={homeStyles.todoListContent}
+      />
     </SafeAreaView>
     </LinearGradient>
   );
